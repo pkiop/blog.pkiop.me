c9b6438c084f29e1ea09b1154bf38e244d5a13ef
pkiop <pkiop@naver.com>
2021-02-07 20:39:15 +0900

    Feat: Add about vue and Add mata data

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>About page</h1>
+    <p>this app display about</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About us',
+      meta: [
+        {
+          hid: 'discription',
+          name: 'discription',
+          content: 'best place for dadjokes',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
