adb3163b4c9c550eeca70bb5b200d21745365001
pkiop <pkiop@naver.com>
2021-02-11 00:37:50 +0900

    Feat: Add api get example

--- frontend/pages/article/_id.vue
+++ frontend/pages/article/_id.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>id를 가짐 {{ $route.params.id }}</div>
+  <div>
+    <div>id를 가짐 {{ $route.params.id }}</div>
+    <div>API Return : {{ info }}</div>
+  </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      info: null,
+    }
+  },
+  async fetch() {
+    await axios
+      .get(`http://localhost:4000/api/posts/12`)
+      .then((response) => (this.info = response.data))
+  },
+}
+</script>
