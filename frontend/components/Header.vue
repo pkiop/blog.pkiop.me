a1092aaa37d46b1fd654258c908a89d84e3c5049
pkiop <pkiop@naver.com>
2021-02-13 22:31:43 +0900

    Feat: Delete test link

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -3,8 +3,6 @@
     <div>App</div>
     <NuxtLink to="/">Home page</NuxtLink>
     <NuxtLink to="/article">article</NuxtLink>
-    <NuxtLink to="/article/1">article 1</NuxtLink>
-    <NuxtLink to="/article/2">article 2</NuxtLink>
   </header>
 </template>
 
