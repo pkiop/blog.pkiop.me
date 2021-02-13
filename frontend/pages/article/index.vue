0599e6a4463b30b2b4abb7321255dc793f1af39d
pkiop <pkiop@naver.com>
2021-02-13 22:32:11 +0900

    Feat: Add test data list / links

--- frontend/pages/article/index.vue
+++ frontend/pages/article/index.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>article main</div>
+  <div>
+    <div>article main</div>
+    <ul id="example-1">
+      <li v-for="list in info" :key="list.id">
+        <NuxtLink :to="`/article/${list.id}`">
+          ID : {{ list.id }} Data : {{ list.name }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      info: null,
+      links: 1,
+    }
+  },
+  async fetch() {
+    this.info = await this.$http.$get(`http://localhost:4000/api/posts`)
+  },
+  fetchOnServer: true,
+}
+</script>
