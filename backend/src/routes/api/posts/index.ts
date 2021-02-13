05702525de2f3409c9e4deae90ad0330a1299c9c
pkiop <pkiop@naver.com>
2021-02-13 22:31:31 +0900

    Feat: Add test data

--- backend/src/routes/api/posts/index.ts
+++ backend/src/routes/api/posts/index.ts
@@ -2,9 +2,31 @@ import { Router } from 'express';
 
 const router = Router();
 
+const data = [
+  {
+    id: 1,
+    name: 'hello',
+  },
+  {
+    id: 2,
+    name: 'bye',
+  },
+  {
+    id: 3,
+    name: 'nice',
+  },
+];
+
+router.get('/', (req, res) => {
+  res.send(data);
+});
+
 router.get('/:id', (req, res) => {
   const clientRequestId = req.params.id;
-  res.send(`My id is ${clientRequestId} HEHE`);
+  const resultData = data.find((el: any) => {
+    return el.id === Number(clientRequestId);
+  });
+  res.send(resultData);
 });
 
 export default router;
