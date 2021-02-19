6d9f9a9978fd3a639ce9a4658ec849fab48fea58
pkiop <pkiop@naver.com>
2021-02-19 22:31:07 +0900

    Feat: Add global reset css

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -20,7 +20,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/scss/reset.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -62,7 +62,7 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ['~/assets/scss/variables.scss'],
     theme: {
       dark: true,
       themes: {
