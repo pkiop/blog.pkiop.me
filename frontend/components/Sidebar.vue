e5db7daf159230fb23ed0712df7610ac38da9e44
pkiop <pkiop@naver.com>
2021-02-23 22:16:43 +0900

    Feat: Add sidebar

---
+++ frontend/components/Sidebar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sidebar">사이드바</div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  data() {
+    return {
+      titleImage: '1',
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.sidebar {
+  position: absolute;
+  display: flex;
+  width: $sidebar-width;
+  @media only screen and (max-width: 768px) {
+    width: $sidebar-width-mobile;
+  }
+  height: 100%;
+  background-color: white;
+}
+</style>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,18 +1,35 @@
 <template>
   <div>
     <Header />
-    <nuxt />
+    <div class="contents">
+      <Sidebar />
+      <nuxt />
+    </div>
     <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header';
+import Sidebar from '@/components/Sidebar';
 import Footer from '@/components/Footer';
+
 export default {
   components: {
     Header,
+    Sidebar,
     Footer,
   },
 };
 </script>
+
+<style lang="scss">
+.contents {
+  display: flex;
+  position: relative;
+
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+</style>
