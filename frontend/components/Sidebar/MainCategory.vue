c4ccd69ce678dbef3763652d8b836dab8d5fb272
pkiop <pkiop@naver.com>
2021-02-25 23:07:33 +0900

    Feat: Add partial category component

---
+++ frontend/components/Sidebar/MainCategory.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="main-category">
+    <div>{{ mainCategory.name }}</div>
+    <SubCategory
+      v-for="subCategory in mainCategory.sub"
+      :key="subCategory.id"
+      :sub-category="subCategory"
+    />
+  </div>
+</template>
+
+<script>
+import SubCategory from '@/components/Sidebar/SubCategory';
+export default {
+  name: 'MainCategory',
+  components: {
+    SubCategory,
+  },
+  props: {
+    mainCategory: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.main-category {
+  margin-top: 2rem;
+  margin-left: 2rem;
+  div {
+    color: black;
+  }
+}
+</style>
---
+++ frontend/components/Sidebar/SubCategory.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sub-category">
+    {{ subCategory.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainCategory',
+  props: {
+    subCategory: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.sub-category {
+  margin-top: 0.4rem;
+  margin-left: 1rem;
+  div {
+    color: black;
+  }
+}
+</style>
