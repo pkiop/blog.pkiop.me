bffd84743a28681b7c59828c634e78251a01b4f9
pkiop <pkiop@naver.com>
2021-03-03 23:09:56 +0900

    Feat: Add sidebar animation

--- frontend/components/Sidebar.vue
+++ frontend/components/Sidebar.vue
@@ -1,21 +1,27 @@
 <template>
-  <div class="sidebar">
-    <MainCategory
-      v-for="mainCategory in category"
-      :key="mainCategory.id"
-      class="main-category"
-      :main-category="mainCategory"
-    />
-  </div>
+  <fragment>
+    <input id="sidebar" type="checkbox" />
+    <div class="sidebar">
+      <label class="on-off" for="sidebar">X</label>
+      <MainCategory
+        v-for="mainCategory in category"
+        :key="mainCategory.id"
+        class="main-category"
+        :main-category="mainCategory"
+      />
+    </div>
+  </fragment>
 </template>
 
 <script>
 import MainCategory from '@/components/Sidebar/MainCategory.vue';
+import { Fragment } from 'vue-fragment';
 
 export default {
   name: 'Sidebar',
   components: {
     MainCategory,
+    Fragment,
   },
   props: {
     category: {
@@ -28,11 +34,35 @@ export default {
 
 <style lang="scss">
 .sidebar {
+  height: 100%;
+  position: relative;
+
   display: flex;
   flex-direction: column;
 
   width: $sidebar-width;
 
   background-color: $color-main2;
+  left: 0;
+  transition: 1s all;
+  .on-off {
+    position: absolute;
+    top: 1%;
+    right: 5%;
+  }
+}
+
+#sidebar {
+  width: 0px;
+  height: 0px;
+  padding: 0px;
+  margin: 0px;
+}
+
+#sidebar:checked ~ .sidebar {
+  background-color: black;
+  left: calc(50px - #{$sidebar-width});
+  color: black;
+  transition: 1s all;
 }
 </style>
