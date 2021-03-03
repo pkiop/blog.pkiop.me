2f9d191971ed3ad354027289c316234c8eff5eaf
pkiop <pkiop@naver.com>
2021-03-03 21:41:46 +0900

    Fix: Fix getDay to getDate

--- frontend/utils/datetime/index.ts
+++ frontend/utils/datetime/index.ts
@@ -1,8 +1,9 @@
 export const reduceStringTime = (time: string) => {
   const date = new Date(time);
+  console.log('date : ', date);
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
-  const day = date.getDay() + 1;
+  const day = date.getDate();
   const hour = date.getHours();
   const min = date.getMinutes();
   const sec = date.getSeconds();
