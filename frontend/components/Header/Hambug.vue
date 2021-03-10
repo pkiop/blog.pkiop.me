9caf752b0c8ff5c2a30ec4a47f76000b3e9e1fa3
pkiop <pkiop@naver.com>
2021-03-10 22:10:28 +0900

    Feat: Add hambug button function in header bar

--- frontend/components/Header/Hambug.vue
+++ frontend/components/Header/Hambug.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="header-hambug">
-    <div v-for="link in linkList" :key="link.id" class="header-hambug-block">
-      <a v-if="link.type === 'a'" :href="link.url"> {{ link.text }} </a>
-      <NuxtLink v-if="link.type === 'nuxtlink'" :to="link.url">
-        {{ link.text }}
-      </NuxtLink>
+    <input id="hambug" type="checkbox" />
+    <div class="header-dropdown">
+      <label class="hambug-on-off" for="hambug" />
+      <div class="hambug-click-dropdown">
+        <div
+          v-for="link in linkList"
+          :key="link.id"
+          class="header-hambug-block"
+        >
+          <a v-if="link.type === 'a'" :href="link.url"> {{ link.text }} </a>
+          <NuxtLink v-if="link.type === 'nuxtlink'" :to="link.url">
+            {{ link.text }}
+          </NuxtLink>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -36,14 +46,50 @@ export default {
   position: absolute;
   top: 0.2rem;
   right: 0.2rem;
+  height: 3rem;
+  width: 3rem;
 
   display: flex;
   flex-direction: column;
-  background-color: skyblue;
+
+  #hambug {
+    width: 0px;
+    height: 0px;
+    padding: 0px;
+    margin: 0px;
+  }
 
   .header-hambug-block {
-    height: 3rem;
     font-size: 2rem;
+    padding: 0.6rem;
+    a {
+      font-size: 0;
+    }
+  }
+
+  #hambug:checked ~ .header-dropdown {
+    a {
+      font-size: 2rem;
+    }
+  }
+
+  .hambug-click-dropdown {
+    position: absolute;
+
+    top: 3rem;
+    right: 0.2rem;
+    background-color: black;
+  }
+
+  .hambug-on-off {
+    background-image: url('~@/assets/hambug.png');
+    display: inline-block;
+    cursor: pointer;
+    line-height: 22px;
+    padding-left: 2.6rem;
+    padding-top: 2.6rem;
+    background-size: contain;
+    background-repeat: no-repeat;
   }
 
   z-index: 10;
