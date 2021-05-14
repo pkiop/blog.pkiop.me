fb8877100d1bcca926bd457c3db8058a728fd12f
pkiop <pkiop@naver.com>
2021-05-14 22:49:33 +0900

    Fix: Eject plugin-sitemap cause error

--- client/blog.pkiop.me/gatsby-config.js
+++ client/blog.pkiop.me/gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
       },
     },
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap',
+    // 'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
