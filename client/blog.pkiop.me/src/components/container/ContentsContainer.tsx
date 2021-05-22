fed4c22b3a652c2576901ebe9bbe3a54ec18cf92
pkiop <pkiop@naver.com>
2021-05-22 15:40:20 +0900

    Feat: Get posts using md remarker

--- client/blog.pkiop.me/src/components/container/ContentsContainer.tsx
+++ client/blog.pkiop.me/src/components/container/ContentsContainer.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import ContentsList from 'components/UI/ContentsList';
+import { graphql, useStaticQuery } from 'gatsby';
 
 function ContentsContainer() {
-  return <div>'콘텐츠가 들어갈 자리'</div>;
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+  const contentsList = data.allMarkdownRemark.edges.map(
+    (el: any) => el.node.frontmatter
+  );
+
+  return (
+    <div>
+      <div>콘텐츠가 들어갈 자리</div>
+      <ContentsList contentsList={contentsList} />
+    </div>
+  );
 }
 
 export default ContentsContainer;
