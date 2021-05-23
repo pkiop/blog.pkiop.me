1cd61efd8b66e03ba1ddb39dd0590afa46ca823a
pkiop <pkiop@naver.com>
2021-05-23 22:54:04 +0900

    Feat: Add global theme and css

--- client/blog.pkiop.me/src/pages/{MarkdownRemark.frontmatter__slug}.tsx
+++ client/blog.pkiop.me/src/pages/{MarkdownRemark.frontmatter__slug}.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import GlobalThemeProvider from 'styles/GlobalThemeProvider';
+import 'styles/post.scss';
+
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }: any) {
   const { markdownRemark } = data; // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark;
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <GlobalThemeProvider>
+      <div className="blog-post-container">
+        <div className="blog-post">
+          <h1>{frontmatter.title}</h1>
+          <h2>{frontmatter.date}</h2>
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
       </div>
-    </div>
+    </GlobalThemeProvider>
   );
 }
 export const pageQuery = graphql`
