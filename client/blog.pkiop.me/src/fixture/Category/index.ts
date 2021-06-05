534c2dc6b06e5e9d568d883dc902827010cf1639
pkiop <pkiop@naver.com>
2021-06-05 14:19:50 +0900

    Feat: Add category

--- client/blog.pkiop.me/src/fixture/Category/index.ts
+++ client/blog.pkiop.me/src/fixture/Category/index.ts
@@ -2,7 +2,13 @@ import { ISubCategory } from 'components/UI/Sidebar/Category/SubCategory';
 import { ICategory } from 'components/UI/Sidebar/Category';
 
 export const subCategoryList1: ISubCategory = ['blog.pkiop.me', 'lifemanager'];
-export const subCategoryList2: ISubCategory = ['javascript', '기타'];
+export const subCategoryList2: ISubCategory = ['개발', '돈', '자기계발'];
+export const thinkCategoryList: ISubCategory = [
+  '개발',
+  '인생',
+  '인간관계',
+  '돈',
+];
 
 export const testCategoryList: ICategory[] = [
   {
@@ -14,5 +20,10 @@ export const testCategoryList: ICategory[] = [
     mainCategory: '책',
     emoji: '📚',
     subCategory: subCategoryList2,
+  },
+  {
+    mainCategory: '생각',
+    emoji: '🤔',
+    subCategory: thinkCategoryList,
   },
 ];
