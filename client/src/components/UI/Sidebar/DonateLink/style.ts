4339e2421bedfa4a22272076ecef11769334d5e5
pkiop <pkiop@naver.com>
2021-06-11 00:23:10 +0900

    Feat: Add DonateLink component

---
+++ client/src/components/UI/Sidebar/DonateLink/style.ts
@@ -0,0 +1,11 @@
+// styles
+import styled from 'styled-components';
+
+export const DonateLink = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > img {
+    width: 80%;
+  }
+`;
