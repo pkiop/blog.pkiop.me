cca62b77e5eea385f63edadfd9f1968488f15536
pkiop <pkiop@naver.com>
2021-06-14 23:57:31 +0900

    Fix: delete unuse import

--- client/src/components/UI/Headerbar/index.tsx
+++ client/src/components/UI/Headerbar/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from 'images/logo.png';
 import Nav from './Nav';
 
 import * as S from './style';
@@ -7,9 +6,7 @@ import * as S from './style';
 function Headerbar() {
   return (
     <S.HeaderBar>
-      <a href="/">
-        PKIOP BLOG
-      </a>
+      <a href="/">PKIOP BLOG</a>
       <Nav />
     </S.HeaderBar>
   );
