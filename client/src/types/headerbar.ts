f52b5130aa56c23c5acd68a34182a6e9b250847c
pkiop <pkiop@naver.com>
2021-07-23 22:51:51 +0900

    types: Add headerbar type

---
+++ client/src/types/headerbar.ts
@@ -0,0 +1,13 @@
+export interface INavButton {
+  text: string;
+  link: string;
+  isClientRoute: boolean;
+}
+
+export interface INav {
+  NavButtonList: INavButton[];
+}
+
+export interface IHeaderbar {
+  nav: INav;
+}
