6e83bd09517a9c98eec56a592308e89143d7dc54
pkiop <pkiop@naver.com>
2021-07-27 22:14:48 +0900

    Chore: Delete deprecated

--- deprecated/backend/src/routes/api/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import postsRouter from './posts';
-
-const router = Router();
-
-router.use('/posts', postsRouter);
-
-export default router;
--- deprecated/backend/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import ApiRouter from './api';
-
-const router = Router();
-
-router.use('/api', ApiRouter);
-
-export default router;
