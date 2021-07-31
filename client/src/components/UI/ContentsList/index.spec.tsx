ab1932251c62a78d5142b01618379c894ec00ead
pkiop <pkiop@naver.com>
2021-07-31 23:53:29 +0900

    Contents: Add contentlist unit test

--- client/src/components/UI/ContentsList/index.spec.tsx
+++ client/src/components/UI/ContentsList/index.spec.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { testContentList } from 'fixture/ContentsList';
 
-// You have to write data-testid
-const Title = () => <h1 data-testid="hero-title">Gatsby is awesome!</h1>;
+import Content from '.';
 
 test('Displays the correct title', () => {
-  const { getByTestId } = render(<Title />);
+  const { container } = render(<Content contentsList={testContentList} />);
   // Assertion
-  expect(getByTestId('hero-title')).toHaveTextContent('Gatsby is awesome!');
+  expect(container.firstChild).toHaveTextContent('글 1');
   // --> Test will pass
 });
