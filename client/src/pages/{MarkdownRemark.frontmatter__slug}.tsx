7fb9ae964426d8e1126870c792cf097f004f175b
pkiop <pkiop@naver.com>
2021-08-12 22:35:58 +0900

    Posting: Add ads

--- client/src/pages/{MarkdownRemark.frontmatter__slug}.tsx
+++ client/src/pages/{MarkdownRemark.frontmatter__slug}.tsx
@@ -36,6 +36,30 @@ export default function Template({
       <PostWrapper>
         <Posts frontmatter={frontmatter} html={html} />
       </PostWrapper>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <iframe
+          src="https://ads-partners.coupang.com/widgets.html?id=488028&template=carousel&trackingCode=AF1679783&subId=&width=680&height=140"
+          width="680"
+          height="140"
+          frameBorder="0"
+          scrolling="no"
+          referrerPolicy="unsafe-url"
+        ></iframe>
+        <a
+          style={{ color: '#539bf5', fontSize: '1.3rem', margin: '1.8rem' }}
+          href="https://coupa.ng/b5eXs7"
+        >
+          이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를
+          제공받습니다.
+        </a>
+      </div>
+
       <Utterances />
     </Wrapper>
   );
