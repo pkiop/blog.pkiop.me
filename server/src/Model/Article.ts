14b06a9c73a2b31cdc90c525d20f26d9a669b297
pkiop <pkiop@naver.com>
2021-11-05 01:37:37 +0900

    Add category model

--- server/src/Model/Article.ts
+++ server/src/Model/Article.ts
@@ -1,1 +1,20 @@
-class ArticleModel {}
+import Model from '../lib/Model/index.ts';
+import { MainCategory, SubCategory } from '../types/category.ts';
+
+type ArticleColumns = {
+  tag: string[];
+  title: string;
+  summary: string;
+  mainCategory: MainCategory;
+  subCategory: SubCategory;
+  readTime: number;
+};
+export default class ArticleModel extends Model {
+  constructor() {
+    super();
+  }
+
+  reload() {
+    console.log('reload');
+  }
+}
