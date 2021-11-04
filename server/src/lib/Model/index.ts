63efe8e9122c04f403c5443553052283d2e468af
pkiop <pkiop@naver.com>
2021-11-05 01:37:01 +0900

    Model: Add mysql connection

--- server/src/lib/Model/index.ts
+++ server/src/lib/Model/index.ts
@@ -1,1 +1,23 @@
-class Model {}
+import { Client } from 'https://deno.land/x/mysql/mod.ts';
+import 'https://deno.land/x/dotenv/load.ts';
+
+type MysqlClient = any;
+
+export default abstract class Model {
+  protected dbClient: MysqlClient;
+  constructor() {
+    this.connection();
+  }
+
+  private async connection() {
+    this.dbClient = await new Client().connect({
+      hostname: Deno.env.get('MYSQL_HOSTNAME'),
+      username: Deno.env.get('MYSQL_USERNAME'),
+      password: Deno.env.get('MYSQL_PASSWORD'),
+      db: Deno.env.get('MYSQL_DBNAME'),
+    });
+    this.dbClient.execute(`CREATE DATABASE IF NOT EXISTS TEST`);
+  }
+
+  public abstract reload(): void; // get full data on db
+}
