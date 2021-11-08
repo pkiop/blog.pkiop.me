66e3e90a390db848ccd8e3e825c4cb744aad3257
pkiop <pkiop@naver.com>
2021-11-09 01:51:12 +0900

    Add body parser

---
+++ server/src/lib/BodyParser/index.ts
@@ -0,0 +1,29 @@
+type ParsedBody = {
+  [key: string]: string;
+};
+
+class BodyParser {
+  constructor() {}
+
+  static async parse(body: ReadableStream<Uint8Array>): Promise<ParsedBody> {
+    const reader = body.getReader();
+    let data = '';
+    while (true) {
+      const readData = await reader.read();
+      console.log('readData : ', readData);
+      if (readData.done) {
+        const keyValues = data.split('&');
+        const result = keyValues.reduce((acc: ParsedBody, keyValue: string) => {
+          const [key, value] = keyValue.split('=');
+          return { ...acc, [key]: value };
+        }, {});
+        return new Promise((resolved) => resolved(result));
+      }
+      readData.value.forEach(
+        (Uint8Data: number) => (data += String.fromCharCode(Uint8Data))
+      );
+    }
+  }
+}
+
+export default BodyParser;
