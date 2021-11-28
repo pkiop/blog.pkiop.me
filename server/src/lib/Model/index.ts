3ed6067c4e1b08d0bc7899fc779fac17caf8fb91
pkiop <pkiopb@gmail.com>
2021-11-28 20:29:08 +0900

    Feat: Add query method

--- server/src/lib/Model/index.ts
+++ server/src/lib/Model/index.ts
@@ -4,19 +4,24 @@ import 'https://deno.land/x/dotenv/load.ts';
 type MysqlClient = any;
 
 export default abstract class Model {
-  protected dbClient: MysqlClient;
+  protected dbClient: MysqlClient | null;
   constructor() {
     this.connection();
   }
 
   private async connection() {
+    this.dbClient = null;
     this.dbClient = await new Client().connect({
       hostname: Deno.env.get('MYSQL_HOSTNAME'),
       username: Deno.env.get('MYSQL_USERNAME'),
       password: Deno.env.get('MYSQL_PASSWORD'),
       db: Deno.env.get('MYSQL_DBNAME'),
     });
-    this.dbClient.execute(`CREATE DATABASE IF NOT EXISTS TEST`);
+  }
+
+  protected async query(sql: string, params: any[] = []) {
+    if (!this.dbClient) throw new Error('NOT CONNECTED');
+    return this.dbClient.execute(sql, params);
   }
 
   public abstract reload(): void; // get full data on db
