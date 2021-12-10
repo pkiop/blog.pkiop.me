bb9ed424a4d7d220bfbe23774ef428917d917ada
pkiop <pkiopb@gmail.com>
2021-12-10 13:59:12 +0900

    Feat: get article using api

--- client-next/src/components/container/ContentsContainer.tsx
+++ client-next/src/components/container/ContentsContainer.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
 import ContentsList from '@components/UI/ContentsList';
 import { ITag } from '@components/UI/Sidebar/TagList/Tag';
 import { testTagList } from '@fixture/Tag';
+import { getArticles } from '@api/index';
 
 const ContentsContainer = () => {
-  const contentsList = [].map((edge: any) => ({
-    ...edge.node.frontmatter,
-    tag: edge.node.frontmatter.tag.map((tag: string) => ({
+  const [articles, setArticles] = useState<any>([]);
+  const fetchData = async () => {
+    const res = await getArticles();
+    setArticles(res);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const contentsList = articles.map((edge: any) => ({
+    ...edge,
+    tags: edge.tags.map((tag: string) => ({
       ...(testTagList.find((testTag: ITag) => testTag.title === tag) || {
         color: 'gray',
         textColor: '#010101',
