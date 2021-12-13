7b4bfc89944ed40ebe334856f92cb2b00155ac35
pkiop <pkiopb@gmail.com>
2021-12-14 03:46:03 +0900

    Feat: Add getArticleBySlug

--- client-next/src/api/index.ts
+++ client-next/src/api/index.ts
@@ -14,10 +14,33 @@ export const getArticles = async () => {
           }
           slug
           tags
+          contents
+          updateAt
         }
       }`,
   });
   return response.data.data.article;
+};
+
+export const getArticleBySlug = async (slug: string) => {
+  const response = await axios.post('/graphql', {
+    query: `query {
+        getArticle(filter: "${slug}") {
+          title
+          mainCategory {
+            title
+          }
+          subCategory {
+            title
+          }
+          slug
+          tags
+          contents
+          updateAt 
+        }
+      }`,
+  });
+  return response.data.data.getArticle;
 };
 
 export default {};
