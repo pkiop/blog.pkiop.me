051086dd6830ab18c0e63c759a1fd7b4894d7a14
pkiop <pkiopb@gmail.com>
2022-01-10 07:35:21 +0900

    Feat: Add getLocaleStringYMD

---
+++ admin/src/utils/date.ts
@@ -0,0 +1,12 @@
+export const getLocaleStringYMD = () => {
+  return new Date()
+    .toLocaleString()
+    .split(',')[0]
+    .split('/')
+    .map((d) => d.padStart(2, '0'))
+    .reduce((acc, d, idx) => {
+      if (idx === 0) return acc + d;
+      if (idx === 1) return acc + '-' + d;
+      return d + '-' + acc;
+    }, '');
+};
