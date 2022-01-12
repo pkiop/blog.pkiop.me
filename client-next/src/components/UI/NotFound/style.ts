bb8e20862a1e0f0ef29754f7fa323dea54c0f0ae
pkiop <pkiopb@gmail.com>
2022-01-13 07:33:30 +0900

    Feat: Add notfound page

---
+++ client-next/src/components/UI/NotFound/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const NotFound = styled.div`
+  height: 80vh;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > h1 {
+    font-size: 3rem;
+  }
+`;
+
+export default NotFound;
